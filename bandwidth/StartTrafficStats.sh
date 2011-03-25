398eede44083aa8982a255196fcafde96e0edcfe
Jarrod Sawyer <sawyer@ubuntu.(none)>
2011-03-24 17:41:19 -0700

    Traffic Statistics start script.

--- bandwidth/StartTrafficStats.sh
+++ bandwidth/StartTrafficStats.sh
@@ -3,9 +3,7 @@
 killall -s USR2 iptraf
 iptraf -i all -f -B -L ./testlogfile.txt
 #sudo chmod a+rw logfile.txt
-echo !!!
-while [ 1 ]
-do
-	python trafficStats.py teams.cfg testlogfile.txt 
-	echo "" > logfile.txt
-done
+
+python trafficStats.py teams.cfg testlogfile.txt 
+
+
