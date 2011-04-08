24bc1200cd9266b1fc3bea352041d8bc324a9dc0
Jarrod Sawyer <jarrod.sawyer@gmail.com>
2011-04-08 11:39:53 -0500

    Moved the teams.cfg file to the main folder and changed the paths to it in the startup script. Created an AttackDetection folder. Moved the createRTDB.sql to the database folder.

--- TrafficStatistics/StartTrafficStats.sh
+++ TrafficStatistics/StartTrafficStats.sh
@@ -4,6 +4,6 @@ killall -s USR2 tcpdump # Kill all curently running tcpdump calls
 
 tcpdump -i any -nn > ./testlogfile.txt & 
 
-python trafficStats.py teams.cfg testlogfile.txt 
+python trafficStats.py ../teams.cfg testlogfile.txt 
 
 
